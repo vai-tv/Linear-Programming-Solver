10cb35c50a6e782a07a738ac1264eee8c4e51789
vai <138060923+vai-tv@users.noreply.github.com>
2026-03-26 19:28:32 +0000

    Create input.py

---
+++ simplex/input.py
@@ -0,0 +1,10 @@
+"""
+Module responsible for handling input for the Simplex problem.
+
+Uses a Tkinter window to receive linear inequality constraints and the P function.
+"""
+
+import tkinter as tk
+
+class GUI(tk.Tk):
+    pass
